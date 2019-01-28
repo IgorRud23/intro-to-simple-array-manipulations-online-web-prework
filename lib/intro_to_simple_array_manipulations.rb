fc5bbebaafbb3f01e3445f40c6d68a1a8cb07e44
Igor Rud <rud.igor23@gmail.com>
2019-01-28 20:44:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,3 +18,7 @@ def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = @chars_in_game_of_thrones
   chars_arya_killed = @chars_in_game_of_thrones.pop 2
 end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
