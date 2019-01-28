d7f94ab067a2abeef1b5fb24745a98d63ce76fe4
Igor Rud <rud.igor23@gmail.com>
2019-01-28 20:41:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,5 +16,5 @@ end
 
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = @chars_in_game_of_thrones
-  chars_arya_killed = (@chars_in_game_of_thrones.pop).pop
+  chars_arya_killed = @chars_in_game_of_thrones.pop 2
 end
