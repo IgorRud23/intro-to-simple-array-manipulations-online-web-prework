cd27351be03caa5d4bc41f771cee2c802e67f4cc
Igor Rud <rud.igor23@gmail.com>
2019-01-28 20:38:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,5 +16,5 @@ end
 
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = @chars_in_game_of_thrones
-  @chars_in_game_of_thrones.pop * 2
+  @chars_in_game_of_thrones.pop + 2
 end
